78aa17fe435eaba0c44d4908c86cbe065cdde787
KayTiwari <abhi.kay.tiwari@gmail.com>
2021-12-13 06:25:51 -0700

    small fix to server

--- server/routes/server.js
+++ server/routes/server.js
@@ -4,10 +4,10 @@ const axios = require("axios");
 
 // Axios get call for users
 router.get("/users", async (req, res) => {
-    const results = req.query.results || "20";
-    const page = req.query.page || "1";
+    const results = req.query.results || "12";
+    const currentPage = req.query.page || "1";
     try {
-        const { data } = await axios.get(`https://randomuser.me/api/?page=${page}&results=${results}&seed=abc`);
+        const { data } = await axios.get(`https://randomuser.me/api/?page=${currentPage}&results=${results}&seed=abc`);
         res.send(data);
     } catch (error) {
         throw new Error(error);
